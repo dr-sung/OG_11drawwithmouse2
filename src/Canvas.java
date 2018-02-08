0197741a18398e6521851bb8698778de264d122e
Hong Sung <profsung@gmail.com>
2018-02-08 16:00:58 -0600

    drawing bug fixed

--- src/Canvas.java
+++ src/Canvas.java
@@ -15,6 +15,7 @@ public class Canvas extends JPanel {
     
     @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
         for (MyRectangle r: boxes) {
